a6eaf710146d5dec851a31359aacff89010958d5
Az Zahra Ghaitsani Nabila <zahraghaitsani@student.uns.ac.id>
2025-07-04 22:50:35 +0700

    Menambahkan routing resource Menu dengan auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => redirect()->route('menu.index'));
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('menu', MenuController::class);
+});

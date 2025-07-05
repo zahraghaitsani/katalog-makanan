2b78b0fdb0425be5fad75957217efbf214d5b505
Az Zahra Ghaitsani Nabila <zahraghaitsani@student.uns.ac.id>
2025-07-05 11:11:20 +0700

    Mengganti navbar brand menjadi WWw

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,9 +29,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand fw-bold text-dark" href="{{ url('/') }}" style="color: #040621; font-weight: bold;">
+                    WWw
                 </a>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
